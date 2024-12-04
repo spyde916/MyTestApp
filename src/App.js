217edfad9f405862295a9fed13d9d828a1f83753
MANISH <spyde916@gmail.com>
2024-12-04 15:00:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/login', {
+      const response = await axios.post('my-backend-app-test-b9ebcnapbxcagzdc.australiacentral-01.azurewebsites.net/login', {
         email,
         password,
       });
